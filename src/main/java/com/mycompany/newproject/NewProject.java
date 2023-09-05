f0db57212e4fb376defcf1530ddf61f09598fec0
CINTIA ARTIGAS PEREIRA <cintia.3990@aluno.pr.senac.br>
2023-09-05 16:23:14 -0300

    primeira alteração

--- src/main/java/com/mycompany/newproject/NewProject.java
+++ src/main/java/com/mycompany/newproject/NewProject.java
@@ -11,6 +11,6 @@ package com.mycompany.newproject;
 public class NewProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Esta alterado!!");
     }
 }
